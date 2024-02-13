60aec7d6e267aa7be9ef7ff36b43957896665fc8
Nick <koskelo2@illinois.edu>
2024-02-13 08:41:50 -0600

    Splitting the global

--- examples/summa.py
+++ examples/summa.py
@@ -16,6 +16,7 @@ from arraycontext import PytatoPyOpenCLArrayContext
 from pyopencl.tools import ImmediateAllocator
 import arraycontext
 import pytools
+from typing import Union
 
 class MyArrayContext(PytatoPyOpenCLArrayContext):
 
@@ -34,8 +35,12 @@ class MyTagType(pytools.tag.Tag):
     def __init__(self, array_location):
         self.array_location = array_location
 
+    def get_location(self):
+        return self.array_location
+
     def __repr__(self):
         return "P" + str(self.array_location)
+
 
 class Mapper:
     def __init__(self, dag_tagged_arrays):
@@ -117,11 +122,13 @@ def main():
                     input2 = B_blocks[k_op_block, jb][k_op]
                     tagged1 = input1.tagged([MyTagType(processor_for_A)])
                     tagged2 = input2.tagged([MyTagType(processor_for_B)])
+
                     op = outer(
                         A_blocks[ib, k_op_block][:, k_op],
                         B_blocks[k_op_block, jb][k_op])
                     C_blocks[ib, jb] = C_blocks[ib, jb] + op
                     C_blocks[ib, jb] = C_blocks[ib, jb].tagged([MyTagType(processor_for_C)])
+                    print("C_blocks[ib,jb[: ",ib,jb,C_blocks[ib, jb])
 
     myMapper = transform.Mapper()
     for ib in range(C_blocks.shape[0]):
@@ -129,9 +136,113 @@ def main():
            print("")
            print(len(C_blocks[ib,jb][0]))
            print((pt.to_index_lambda(C_blocks[ib,jb][0][0]).bindings))
+           print(type(C_blocks[ib,jb]))
+           print(C_blocks[ib,jb].tags)
            print("")
+
+
+    print((C_blocks[0,0]))
+
+
+    my_scheduler_send_nodes = [[] for i in range(9)]
+    build_send_lists(C_blocks[0,0],my_scheduler_send_nodes)
+
+
+    print(my_scheduler_send_nodes)
+
     C_blocks_host = actx.to_numpy(actx.freeze(C_blocks))
     #print(C_blocks_host)
-    
+
+
+def build_send_lists(array, proc_ids_to_comm_ids):
+
+
+    stack = [array]
+    cnt = 0
+    while len(stack) > 0:
+        next_array = stack.pop()
+        loc = None
+        for tag in next_array.tags:
+            if isinstance(tag, MyTagType):
+                loc = tag.get_location()
+
+        if len(array.bindings) > 0:
+            print("\n",array.tags,"\n")
+            
+            print("the first array should be located at: ",loc)
+            for key in next_array.bindings.keys():
+                your_loc = None
+                descend = next_array.bindings[key]
+                print(descend, "and tags: ", descend.tags)
+                for tag in descend.tags:
+                    print(MyTagType)
+                    print(type(tag))
+                    if type(tag) == MyTagType:
+                         your_loc = tag.get_location()
+                    else:
+                         print("Stated as no match. However, we have: ", MyTagType, " and the instant: ", type(tag))
+                    #if isinstance(tag, MyTagType):
+                    #     your_loc = tag.get_location()
+                #your_loc = next_array.bindings[key].tags.array_location
+                print("first depend is located at: ", your_loc)
+                if your_loc is None:
+                   # is null then we are just going to add it onto the stack.
+                   stack.append(descend)
+                elif loc is None:
+                   # Missing data about the combine step.
+                   stack.append(descend)
+                elif loc != your_loc:
+                    # We are splitting this edge. This means that we need to make a Send/Receive pair.
+                    comm_id = tuple([your_loc, loc, len(proc_ids_to_comm_ids[your_loc]) + 1])
+                    send = pt.make_distributed_send(next_array.bindings[key], loc, comm_id)
+                    receive = pt.make_distributed_recv(your_loc, comm_id, next_array.bindings[key].shape)
+                    proc_ids_to_comm_ids[your_loc].append(send)
+                else:
+                    print(type(next_array.bindings[key]))
+                    if isinstance(next_array.bindings[key], pt.IndexLambda):
+                        stack.append(next_array.bindings[key])
+        cnt += 1
+        print("cnt: ", cnt)
+            # else we are good to keep going on this path.
+    return
+
+def divide(array, my_dag, proc_of_interest: int):
+
+    """ 
+
+    # If array has dependencies we need to check the children.
+    if array has descendants:
+        left = None
+        if array.bindings[0].tags == proc_of_interest:
+            left = divide(array.bindings[0], my_dag, proc_of_interest)
+        else:
+            left = RecieveNodeFromProcessorLEFT
+        
+        right = None
+        if array.bindings[1].tags == proc_of_interest:
+            right = divide(array.bindings[1], my_dag, proc_of_interest)
+        else:
+            right = RecieveNodeFromProcessorRIGHT
+        array.bindings[0] = left
+        array.bindings[1] = right
+        return array
+    # else
+    else:
+        if array.tags:
+            pass
+        return array
+
+
+    #if isinstance(array, BasicIndex):
+        # This is just an array so we can just add it to the dag
+     #   pass
+
+    #elif isinstance(array, IndexLambda):
+        # Looks like we are able to make more deep down. 
+     #   pass
+
+    assert False
+    """
+
 if __name__ == "__main__":
     main()
